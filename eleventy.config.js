ff27b0bdc8af87106d4a71a92f39c53ec0587d62
Deepak Kar <github@deepakk.xyz>
2025-05-07 13:40:05 +0100

    Basic 11ty Setup

---
+++ eleventy.config.js
@@ -0,0 +1,27 @@
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.setInputDirectory("src");
+    eleventyConfig.setOutputDirectory("docs");
+
+    eleventyConfig.addPassthroughCopy("src/**/*.css");
+    eleventyConfig.addPassthroughCopy("src/**/*.pdf");
+
+    eleventyConfig.addFilter("formatDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toISODate();
+    });     
+
+    let markdownOptions = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    let markdownLib = new markdownIt(markdownOptions);
+
+    //Add div around tables
+    markdownLib.renderer.rules.table_open = () => '<div class="table-wrapper">\n<table>\n',
+    markdownLib.renderer.rules.table_close = () => '</table>\n</div>',
+
+    eleventyConfig.setLibrary("md", markdownLib);
+};
